513d1e4ace29090eb7ae509fc5464b3ae3036f09
Nikita199909 <nikitazhuchkevich@gmail.com>
2018-09-16 10:41:48 +0300

    laba 1

---
+++ Laba_1_PCi/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private const string deviceID = "DeviceID";
+        private const string vendorID = "VenderID";
+        private const string pci = "PCI";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            foreach (var obj in GetPciDev().ToArray())
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+
+        public static IEnumerable<string> GetPciDev()
+        {
+            return new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity").Get()
+                .Cast<ManagementBaseObject>()
+                .Select(obj => obj[deviceID])
+                .Cast<string>()
+                .Where(obj => obj.Contains(pci))
+                .Select(str => $"{deviceID}: {str.Substring(17, 4)} {vendorID}: 0x{str.Substring(8, 4)}");
+        }
+    }
+}
